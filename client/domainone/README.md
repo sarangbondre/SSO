f852345037b3ae04208246bb406758c6a8438c78
Sarang <123.sarang@gmail.com>
2015-11-09 11:11:08 +0530

    Added symfony structure for the client

---
+++ client/domainone/README.md
@@ -0,0 +1,4 @@
+domainone
+=========
+
+A Symfony project created on November 9, 2015, 11:03 am.
---
+++ client/domainone/src/AppBundle/Controller/RedirectController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/AppBundle/Controller/RedirectController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
+
+?>
